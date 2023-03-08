3e43298be8c20422b4c8970808b3cb37fb6ba51b
anwerESS <43082478+anwerESS@users.noreply.github.com>
2023-03-09 00:58:43 +0100

    Update tmp.h

--- tmp.h
+++ tmp.h
@@ -1,1 +1,27 @@
 
+#ifndef CURL_REQUEST_H
+#define CURL_REQUEST_H
+
+#include <string>
+#include <curl/curl.h>
+
+class CurlRequest {
+public:
+    CurlRequest(const char *url, const char *json_str);
+    ~CurlRequest();
+
+    void perform();
+
+    long get_response_code() const;
+    const std::string& get_response_body() const;
+
+private:
+    CURL *curl;
+    CURLcode res;
+    struct curl_slist *headers = NULL;
+    std::string response;
+
+    static size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata);
+};
+
+#endif
