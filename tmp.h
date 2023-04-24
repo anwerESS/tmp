61b174f561c6237779de891aed39856b8f7acb22
anwerESS <43082478+anwerESS@users.noreply.github.com>
2023-04-24 19:35:20 +0200

    Update tmp.h

--- tmp.h
+++ tmp.h
@@ -1,19 +1,4 @@
-UPDATE dri
-SET dri.dri_input_date = SYSDATE
-WHERE dri.status = '003' 
-  AND dri.dri_code IN ('1001', '1002')
-  AND EXISTS (
-    SELECT 1 
-    FROM dri_histo 
-    WHERE dri_histo.dri_input_date = dri.dri_input_date
-  );
-
-UPDATE dri_histo
-SET dri_histo.dri_input_date = SYSDATE
-WHERE EXISTS (
-    SELECT 1 
-    FROM dri 
-    WHERE dri.status = '003' 
-      AND dri.dri_code IN ('1001', '1002')
-      AND dri_histo.dri_input_date = dri.dri_input_date
-  );
+SELECT d.dri_code, d.dri_input_date
+FROM dri d
+JOIN dri_histo h ON d.dri_code = h.dri_code AND d.dri_input_date = h.dri_input_date
+WHERE d.sta_id = '003' AND d.dri_code IN ('1001', '1002')
