a7a043100c61aa03df9847cfe002abe847a1d95e
anwerESS <43082478+anwerESS@users.noreply.github.com>
2023-04-24 19:27:31 +0200

    Update tmp.h

--- tmp.h
+++ tmp.h
@@ -1,27 +1,19 @@
+UPDATE dri
+SET dri.dri_input_date = SYSDATE
+WHERE dri.status = '003' 
+  AND dri.dri_code IN ('1001', '1002')
+  AND EXISTS (
+    SELECT 1 
+    FROM dri_histo 
+    WHERE dri_histo.dri_input_date = dri.dri_input_date
+  );
 
-#ifndef CURL_REQUEST_H
-#define CURL_REQUEST_H
-
-#include <string>
-#include <curl/curl.h>
-
-class CurlRequest {
-public:
-    CurlRequest(const char *url, const char *json_str);
-    ~CurlRequest();
-
-    void perform();
-
-    long get_response_code() const;
-    const std::string& get_response_body() const;
-
-private:
-    CURL *curl;
-    CURLcode res;
-    struct curl_slist *headers = NULL;
-    std::string response;
-
-    static size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata);
-};
-
-#endif
+UPDATE dri_histo
+SET dri_histo.dri_input_date = SYSDATE
+WHERE EXISTS (
+    SELECT 1 
+    FROM dri 
+    WHERE dri.status = '003' 
+      AND dri.dri_code IN ('1001', '1002')
+      AND dri_histo.dri_input_date = dri.dri_input_date
+  );
