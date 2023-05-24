24f216dda7d2ed6721a80915b29c33f7419349f3
anwerESS <43082478+anwerESS@users.noreply.github.com>
2023-05-24 10:12:58 +0200

    Update tmp.h

--- tmp.h
+++ tmp.h
@@ -1,4 +1,16 @@
-SELECT d.dri_code, d.dri_input_date
-FROM dri d
-JOIN dri_histo h ON d.dri_code = h.dri_code AND d.dri_input_date = h.dri_input_date
-WHERE d.sta_id = '003' AND d.dri_code IN ('1001', '1002')
+actually, during development. there were no problems.
+SO, we could get the token, the "liason code" and all that.
+
+just , we would like to know when can we safely integrate our development?
+
+is the API still under development?
+
+can we see big changes in the structure of  responses body that could impact our development?
+
+Because we in the code, we need to retrieve "the asociated account" and "bank code" from an "account number".
+
+so we had to make two requests.
+	the first step we the "get the associated securiry account"
+	and then, using this result we retreive the bank code
+
+of course the structure does not pose a problem. we can adapt it.
