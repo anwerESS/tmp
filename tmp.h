abea8425cbe28dc671389415ad9f2623a7292539
anwerESS <43082478+anwerESS@users.noreply.github.com>
2023-05-30 14:52:38 +0200

    Update tmp.h

--- tmp.h
+++ tmp.h
@@ -1,16 +1,99 @@
-actually, during development. there were no problems.
-SO, we could get the token, the "liason code" and all that.
+#include <iostream>
+#include <sstream>
+#include <cstring>
 
-just , we would like to know when can we safely integrate our development?
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <cstdlib>
+#endif
 
-is the API still under development?
+int executeCommand(const std::string& command, std::string& output) {
+#ifdef _WIN32
+    // Create a pipe for command output
+    HANDLE pipeRead, pipeWrite;
+    SECURITY_ATTRIBUTES saAttr;
+    saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+    saAttr.bInheritHandle = TRUE;
+    saAttr.lpSecurityDescriptor = NULL;
 
-can we see big changes in the structure of  responses body that could impact our development?
+    if (!CreatePipe(&pipeRead, &pipeWrite, &saAttr, 0)) {
+        return -1;  // Failed to create pipe
+    }
 
-Because we in the code, we need to retrieve "the asociated account" and "bank code" from an "account number".
+    // Set up STDOUT to write to the pipe
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+    si.hStdOutput = pipeWrite;
+    si.dwFlags |= STARTF_USESTDHANDLES;
 
-so we had to make two requests.
-	the first step we the "get the associated securiry account"
-	and then, using this result we retreive the bank code
+    // Execute the command
+    if (!CreateProcess(NULL, const_cast<char*>(command.c_str()), NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) {
+        CloseHandle(pipeRead);
+        CloseHandle(pipeWrite);
+        return -1;  // Failed to execute the command
+    }
 
-of course the structure does not pose a problem. we can adapt it.
+    // Close the write end of the pipe
+    CloseHandle(pipeWrite);
+
+    // Read the command output from the pipe
+    DWORD bytesRead;
+    const int bufferSize = 4096;
+    char buffer[bufferSize];
+
+    while (true) {
+        if (!ReadFile(pipeRead, buffer, bufferSize - 1, &bytesRead, NULL) || bytesRead == 0) {
+            break;  // No more data or read error
+        }
+
+        buffer[bytesRead] = '\0';
+        output += buffer;
+    }
+
+    // Close the read end of the pipe
+    CloseHandle(pipeRead);
+
+    // Get the exit code of the process
+    DWORD exitCode;
+    GetExitCodeProcess(pi.hProcess, &exitCode);
+
+    // Close process and thread handles
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    return static_cast<int>(exitCode);
+
+#else  // For non-Windows platforms (Linux, macOS, etc.)
+
+    // Execute the command and capture the output using popen()
+    FILE* pipe = popen(command.c_str(), "r");
+    if (!pipe) {
+        return -1;  // Failed to execute the command
+    }
+
+    char buffer[128];
+    while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
+        output += buffer;
+    }
+
+    int exitCode = pclose(pipe);
+
+    return WIFEXITED(exitCode) ? WEXITSTATUS(exitCode) : -1;
+
+#endif
+}
+
+int main() {
+    std::string command = "ls";  // Replace with your desired command
+
+    std::string output;
+    int exitCode = executeCommand(command, output);
+
+    std::cout << "Exit code: " << exitCode << std::endl;
+    std::cout << "Command output:\n" << output << std::endl;
+
+    return 0;
+}
