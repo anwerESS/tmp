ddba5ea79d2358dcc363294a66d825ae4b27bd88
anwerESS <43082478+anwerESS@users.noreply.github.com>
2023-06-15 01:08:53 +0200

    Update tmp.h

--- tmp.h
+++ tmp.h
@@ -1,3 +1,29 @@
+curl --request POST \
+  --url https://sgconnect-hom.fr.world.socgen/sgconnect/oauth2/access_token \
+  --header 'Authorization: Basic YWI4MzQwMmEtMmRiMi00NmExLTkyZWMtYzVjODg1NmMzYTlmOjJiMDZoNG45ZGU5OGpjbWZobDFuY2g3a2I1MDJk' \
+  --header 'Content-Type: application/x-www-form-urlencoded' \
+  --data grant_type=client_credentials \
+  --data 'scope=api.sgss-institutional-custody-securities-account.v1 api.sgss-institutional-custody-securities-account.securities-accounts.read'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 CURL *hnd = curl_easy_init();
 
 curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "POST");
