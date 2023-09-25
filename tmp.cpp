19b485ee1ee38ae75282f0961fa11bdf00acc9ba
anwerESS <43082478+anwerESS@users.noreply.github.com>
2023-09-25 14:32:55 +0200

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -1,23 +1,21 @@
-std::string extractBankCode(const std::string& json) {
-    // Find the start of "securitiesAccountBasicData" object
-    size_t basicDataStart = json.find("\"securitiesAccountBasicData\": {");
-    if (basicDataStart == std::string::npos) {
-        return ""; // "securitiesAccountBasicData" object not found
+std::string extractBankCode(const std::string &json) {
+    std::string bankCode;
+    size_t pos = json.find("\"bankCode\"");
+    if (pos != std::string::npos) {
+        // Find the colon after "bankCode":
+        size_t colonPos = json.find(":", pos);
+        if (colonPos != std::string::npos) {
+            // Find the opening double quote after the colon:
+            size_t start = json.find("\"", colonPos);
+            if (start != std::string::npos) {
+                // Find the closing double quote after "bankCode" value:
+                size_t end = json.find("\"", start + 1);
+                if (end != std::string::npos) {
+                    // Extract the bankCode value:
+                    bankCode = json.substr(start + 1, end - start - 1);
+                }
+            }
+        }
     }
-
-    // Find the start of "bankCode" key within "securitiesAccountBasicData"
-    size_t bankCodeStart = json.find("\"bankCode\": \"", basicDataStart);
-    if (bankCodeStart == std::string::npos) {
-        return ""; // "bankCode" key not found within "securitiesAccountBasicData"
-    }
-
-    // Find the end of "bankCode" value
-    size_t bankCodeEnd = json.find("\"", bankCodeStart + 14);
-    if (bankCodeEnd == std::string::npos) {
-        return ""; // Unclosed "bankCode" value
-    }
-
-    // Extract and return the "bankCode" value
-    return json.substr(bankCodeStart + 14, bankCodeEnd - bankCodeStart - 14);
+    return bankCode;
 }
- 
