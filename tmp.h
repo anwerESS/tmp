99eb5350d27baef44d1015e7c38daea528caf64b
anwerESS <43082478+anwerESS@users.noreply.github.com>
2023-12-11 19:14:38 +0100

    Update tmp.h

--- tmp.h
+++ tmp.h
@@ -1,18 +1,18 @@
-std::string formatFloat(double d) {
-    std::stringstream stream;
-    stream << std::fixed << std::setprecision(2) << d;
-    std::string s = stream.str();
-    std::string integerPart = s.substr(0, s.find('.'));
-    std::string decimalPart = s.substr(s.find('.') + 1);
-    std::string formattedIntegerPart;
-    int count = 0;
-    for (int i = integerPart.size() - 1; i >= 0; i--) {
-        formattedIntegerPart = integerPart[i] + formattedIntegerPart;
-        count++;
-        if (count == 3 && i != 0) {
-            formattedIntegerPart = " " + formattedIntegerPart;
-            count = 0;
-        }
-    }
-    return formattedIntegerPart + "," + decimalPart;
-}
+Subject: Urgent: Inquiry on ECAR API Changes
+
+Dear [Recipient's Name],
+
+I hope this message finds you well. I am [Your Name], a software developer on the [Your Team/Project Name] team. We rely on the ECAR API for our application, and I need urgent information regarding the upcoming changes your team is making.
+
+Delivery Date to Production:
+Can you please share the expected delivery date for the upcoming ECAR API changes in the production environment?
+
+URL or Endpoint Changes:
+Are there any planned changes to the URL or endpoint structure of the ECAR API?
+
+New Object Format Returned by the API:
+Will there be modifications to the format of objects returned by the ECAR API?
+
+Your prompt response is crucial to ensure a smooth transition. Thank you for your assistance.
+
+Best,
