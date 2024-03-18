9a07f6766199c5d90d90543cd93a38028019f997
anwerESS <43082478+anwerESS@users.noreply.github.com>
2024-03-18 09:19:25 +0100

    Update tmp.h

--- tmp.h
+++ tmp.h
@@ -1,11 +1,12 @@
 def text_to_c_string(long_text, segment_length):
     segments = [long_text[i:i+segment_length] for i in range(0, len(long_text), segment_length)]
-    c_string = '"\n"'.join(segments)
+    escaped_segments = ['\\"' + segment.replace('"', '\\"') + '\\"' for segment in segments]
+    c_string = '"\n"'.join(escaped_segments)
     c_string = '"' + c_string + '"'
     return c_string
 
 # Example usage:
-long_text = "abcdefghijk"
+long_text = 'a"bcd"ef"ghijk'
 segment_length = 5
 c_string = text_to_c_string(long_text, segment_length)
 print(c_string)
