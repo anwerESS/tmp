333e447633d3ee3e959983a3e30644d6194c9964
anwerESS <43082478+anwerESS@users.noreply.github.com>
2024-05-31 10:17:30 -0400

    Update tmp.c

--- tmp.c
+++ tmp.c
@@ -3,8 +3,7 @@ import re
 s = "***** **** (key-1, key-2, key-3, key-n) *** ***** **** (val-1, val-2, val-3, val-n) "
 
 # Find all content within parentheses
-matches = re.findall(r'(?<=\s)\((.*?)\)', s)
-
+matches = re.findall(r'(?<=\s)\(([^()]*?)\)', s)
 # Split the keys and values by commas
 keys = matches[0].split(', ')
 values = matches[1].split(', ')
