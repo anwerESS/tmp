3792301e24dee85dfe60887ebbc286253165b952
anwerESS <43082478+anwerESS@users.noreply.github.com>
2024-11-20 22:06:56 +0100

    Update tmp.c

--- tmp.c
+++ tmp.c
@@ -1,1 +1,164 @@
 
+{
+  "oracledevtools.connectionConfiguration.configFilesFolder": "/home/anweress/Oracle/network/admin",
+  "[oraclesql]": {
+    "editor.suggest.showSnippets": true,
+    "editor.quickSuggestions": {
+      "comments": "on",
+      "strings": "on",
+      "other": "on"
+    }
+  },
+  "oracledevtools.connectionConfiguration.walletFileFolder": "/home/anweress/Oracle/network/admin",
+  "oracledevtools.bookmarkFileFolder": "/home/anweress/Oracle/oracle.oracledevtools",
+  "oracledevtools.download.otherFolder": "/home/anweress/downloads",
+  "workbench.colorTheme": "monokai-charcoal (red)",
+  "redhat.telemetry.enabled": false,
+  "files.autoSave": "afterDelay",
+  "sonarlint.rules": {
+    "css:S125": {
+      "level": "off"
+    },
+    "css:S4658": {
+      "level": "off"
+    },
+    "javascript:S125": {
+      "level": "off"
+    },
+    "javascript:S6774": {
+      "level": "off"
+    },
+    "javascript:S905": {
+      "level": "off"
+    },
+    "javascript:S6848": {
+      "level": "off"
+    },
+    "javascript:S3358": {
+      "level": "off"
+    },
+    "javascript:S1082": {
+      "level": "off"
+    }
+  },
+  "workbench.iconTheme": "material-icon-theme",
+  "frameIndentRainbow.ignoreErrorLanguages": [
+  
+  "markdown"],
+  // "frameIndentRainbow.includedLanguages": [
+  //     "html", "html5", "java",
+  // ],
+  "frameIndentRainbow.colors": [
+    "rgba(0, 229, 255, 0.1)",
+    "rgba(174, 0, 255, 0.1)",
+    "rgba(0, 255, 162, 0.1)",
+    "rgba(239, 255, 64, 0.1)",
+    "rgba(245, 66, 96, 0.1)",
+    "rgba(0, 229, 255, 0.1)",
+    "rgba(174, 0, 255, 0.1)",
+    "rgba(0, 255, 162, 0.1)",
+    "rgba(239, 255, 64, 0.1)"
+  ],
+  "indentRainbow.colors": [
+    "rgba(245, 66, 96, 1)",
+    "rgba(0, 229, 255, 1)",
+    "rgba(174, 0, 255, 1)",
+    "rgba(0, 255, 162, 1)",
+    "rgba(239, 255, 64, 1)",
+    "rgba(245, 66, 96, 1)",
+    "rgba(0, 229, 255, 1)",
+    "rgba(174, 0, 255, 1)",
+    "rgba(0, 255, 162, 1)",
+    "rgba(239, 255, 64, 1)"
+  ],
+  // Using the light mode
+  "indentRainbow.indicatorStyle": "light",
+  // we use a simple 1 pixel wide line
+  "indentRainbow.lightIndicatorStyleLineWidth": 2,
+  "editor.mouseWheelZoom": true,
+  "terminal.integrated.mouseWheelZoom": true,
+  "terminal.integrated.fontSize": 13,
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  },
+  "cmake.showOptionsMovedNotification": false,
+  "explorer.confirmDelete": false,
+  "[javascript]": {
+    "editor.defaultFormatter": "vscode.typescript-language-features"
+  },
+  "editor.indentSize": "tabSize",
+  "editor.tabSize": 2,
+  "git.openRepositoryInParentFolders": "never",
+  "editor.lineHeight": 1.04,
+  "prettier.printWidth": 100,
+  "rsp-ui.enableStartServerOnActivation": [
+    {
+      "id": "redhat.vscode-community-server-connector",
+      "name": "Community Server Connector",
+      "startOnActivation": true
+    }
+  ],
+  "editor.minimap.renderCharacters": false,
+  "editor.minimap.showSlider": "always",
+  "editor.minimap.enabled": false,
+  "[css]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "[html]": {
+    "editor.defaultFormatter": "vscode.html-language-features"
+  },
+  "RainbowBrackets.consecutivePairColors": [
+    "()",
+    "[]",
+    "{}",
+    [
+      "rgba(245, 66, 96, 1)",
+      "rgba(0, 229, 255, 1)",
+      "rgba(174, 0, 255, 1)",
+      "rgba(0, 255, 162, 1)",
+      "rgba(239, 255, 64, 1)"
+    ],
+    "Revioletd"
+  ],
+  "frameIndentRainbow.includedLanguages": [],
+  "frameIndentRainbow.excludedLanguages": ["plaintext"],
+  "indentRainbow.excludedLanguages": ["plaintext"],
+  "RainbowBrackets.depreciation-notice": false,
+  "[javascriptreact]": {
+    "editor.defaultFormatter": "vscode.typescript-language-features"
+  },
+  "material-icon-theme.folders.customClones": [],
+  "[json]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "[typescriptreact]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "editor.formatOnSave": true,
+  "prettier.configPath": ".prettierrc",
+  "prettier.insertPragma": true,
+  "prettier.jsxSingleQuote": true,
+  "prettier.withNodeModules": true,
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+  "notebook.defaultFormatter": "esbenp.prettier-vscode",
+  "prettier.singleQuote": true,
+  "editor.inlineSuggest.suppressSuggestions": true,
+  "styled-colors.styled_colors": ["RGB", "BROWSERS_COLORS", "HEXA", "RGB", "HSL"],
+  "colorize.include": [
+    "**/*.css",
+    "**/*.scss",
+    "**/*.sass",
+    "**/*.less",
+    "**/*.styl",
+    "**/*.js",
+    "**/*.jsx"
+  ],
+  "workbench.editor.customLabels.patterns": {
+    "**/app/**/page.js": "PAGE ${dirname}"
+  },
+  "window.titleBarStyle": "custom",
+  "workbench.layoutControl.enabled": false,
+  "workbench.editor.enablePreview": false,
+  "diffEditor.wordWrap": "on",
+  "editor.wordWrap": "on"
+}
