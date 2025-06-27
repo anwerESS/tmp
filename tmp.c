c7867da1574c61be3c9e944fa065d6ec3777aca9
anwerESS <43082478+anwerESS@users.noreply.github.com>
2025-06-28 01:07:07 +0200

    Update tmp.c

--- tmp.c
+++ tmp.c
@@ -1,1 +1,7 @@
 
+Salut,
+BoursoBank, tu connais ? C'est la banque la moins chère depuis 17 ans* !
+Ouvre ton compte avec un premier versement minimum (voir conditions de l'offre) pour que nous puissions bénéficier tous les deux d'une récompense.
+Je t'invite à cliquer sur ce lien https://bour.so/p/QDmc1fTaBsQ. Mes informations de Parrainage seront automatiquement préremplies dans ton parcours d'ouverture de compte BoursoBank.
+À très vite !
+*Voir détails sur site
