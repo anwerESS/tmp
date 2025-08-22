012362be493b0db6175d2e2a532054b55b48cd9b
anwerESS <43082478+anwerESS@users.noreply.github.com>
2025-08-22 15:57:22 +0200

    Update tmp.c

--- tmp.c
+++ tmp.c
@@ -1,3 +1,1 @@
-Il n’y a rien d’explicite dans les logs indiquant la cause du crash (cela pourrait être un problème de ressources).
-J’ai consulté l’historique et trouvé des cas similaires : Nourredine clôturait le ticket avec la mention :
-“Solution Description: Knowledge article: 843732 - No Procedure
+
