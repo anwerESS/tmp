07d28d49446019441dfd57f9d5b25b62de59b9a8
anwerESS <43082478+anwerESS@users.noreply.github.com>
2025-08-29 09:50:46 +0200

    Update tmp.c

--- tmp.c
+++ tmp.c
@@ -1,1 +1,1 @@
-Remarque : Pour les business rules, les variables déclarées doivent être écrites exactement comme elles le sont dans le code Java. Elles sont sensibles à la casse (case sensitive).
+
