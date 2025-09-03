f04f714b07e71592bbc89d652780ff3598c6aaee
anwerESS <43082478+anwerESS@users.noreply.github.com>
2025-09-03 10:26:52 +0200

    Update tmp.c

--- tmp.c
+++ tmp.c
@@ -1,1 +1,11 @@
+Autosys Access Issue – Request for Updated Application URL
 
+
+
+Hi team,
+
+I’m getting an error when launching Autosys (see attached screenshot).
+Could you please confirm if the Java application URL has recently changed?
+If so, could you share with us the new URL so we can update our configuration?
+
+Thanks in advance!
