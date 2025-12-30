2b0cec3b1cc61293be5aceccf0307fda31599396
anwerESS <43082478+anwerESS@users.noreply.github.com>
2025-12-30 19:57:07 +0100

    Remove farewell messages from tmp.c

--- tmp.c
+++ tmp.c
@@ -1,15 +1,1 @@
-Hello l’équipe E7,
-Je vous écris car aujourd’hui c’est mon dernier jour chez SGSS. Après plus de 3 ans ici, dont les 8 derniers mois avec E7, je tenais à vous dire merci.
-C’était une super occasion pour apprendre, progresser, et travailler dans une équipe au top.
-Merci pour votre soutien, vos échanges, et votre collaboration au quotidien. Je vous souhaite une très bonne continuation et bon courage pour la suite !
 
-=================================================
-Guillame
-Bonjour G,
-Comme tu es en congés et qu’on ne se croisera pas, je voulais te laisser un message : aujourd’hui c’est mon dernier jour chez SGSS.
-Merci pour ton soutien, tes conseils et ta disponibilité pendant cette période. Même si tu n’étais pas directement dans notre équipe, ton aide m’a vraiment servi.
-Je te souhaite de très bonnes vacances et une bonne continuation.
-=================================================
-Bonjour A,
-Aujourd’hui c’est mon dernier jour chez SGSS. Je voulais te remercier pour notre collaboration côté SIB et pour ta disponibilité.
-Bonne continuation et au plaisir !
